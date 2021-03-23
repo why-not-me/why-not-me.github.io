34880c14e5e1123da27750d19a6745bcdbde564d
WangJunMing <3297283294@qq.com>
2021-03-23 10:27:19 +0800

    blog

---
+++ blog/.vuepress/config.js
@@ -0,0 +1,9 @@
+// .vuepress/config.js
+module.exports = {
+  // why-not-me.github.io
+  title: 'WangJunMing', // Title for the site. This will be displayed in the navbar.
+  theme: '@vuepress/theme-blog',
+  themeConfig: {
+    // Please keep looking down to see the available options.
+  }
+};
